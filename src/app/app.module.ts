d7a35f7820f2c42947db4c63535ce251fe5f67a8
Oleh <sluzhalaoleh@gmail.com>
2019-12-09 12:44:30 +0200

    datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxPopper } from 'angular-popper';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NgbdDatepickerBasic } from './datepicker-basic';
+import { NgbdDatepickerPopup } from './datepicker-popup/datepicker-popup';
+import { NgbdDatepickerRangePopup } from './datepicker-rangepopup/datepicker-rangepopup';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdDatepickerBasic,
+    NgbdDatepickerPopup,
+    NgbdDatepickerRangePopup
   ],
   imports: [
+    FormsModule, 
+    NgbModule,
+    NgxPopper,
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [
+    NgbdDatepickerBasic,
+    NgbdDatepickerPopup,
+    NgbdDatepickerRangePopup
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NgbdDatepickerBasic,
+    NgbdDatepickerPopup,
+    NgbdDatepickerRangePopup
+  ]
 })
 export class AppModule { }
