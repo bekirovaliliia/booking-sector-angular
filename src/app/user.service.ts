37645ea07a0b6a0b07e8a07a55646b73ad5bc8a0
Liliia Bekirova <lilybekirova@gmail.com>
2019-12-24 16:41:26 +0200

    added component for password changing

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,9 +10,11 @@ export class UserService {
 
   apiURl = 'https://localhost:44393/api/users';
   constructor(private http: HttpClient) { }
-
   getUser(id:number) {
     return this.http.get<User>(`${this.apiURl}/${id}`);
+  }
+  checkPass(pass: string, id:number){
+   return this.http.get<Boolean>(`${this.apiURl}/${id}/${pass}`);
   }
   updateUser(user :User) {
     const httpOptions = {
