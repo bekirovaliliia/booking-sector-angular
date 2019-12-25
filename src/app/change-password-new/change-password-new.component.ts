e4dec288073023cd40d7403f0b573848fb19fa1a
Liliia Bekirova <lilybekirova@gmail.com>
2019-12-25 23:47:43 +0200

    added button to change password

--- src/app/change-password-new/change-password-new.component.ts
+++ src/app/change-password-new/change-password-new.component.ts
@@ -10,45 +10,78 @@ import {UserService} from '../core/services/user.service';
 })
 export class ChangePasswordNewComponent implements OnInit {
   changePasswordForm:FormGroup;
-  onStrengthChanged(strength: number) {
-    this.passwordStrength = strength;
-    console.log('password strength = ', strength);
-  }
-  user: User;
-  id = 46;
-  passwordStrength: number = 0;
+user: User;
+id = 46;
+passwordStrength: number = 0;
 old: Boolean = false;
 check: boolean = false;
+oldPassword: string = "";
 newPassword: string = "";
 newPasswordConfirm: string = "";
-oldPassColor:string = "primary";
 checked = false;
-  saveChanges(){
-    if(this.passwordStrength<40){ alert("Your password must meet at least 2 of 5 conditions");}
-    else{
-    this.user.password = this.newPassword;
-    this.old = false;
-    this.checked = false;
-    this.userService.updateUser(this.user).subscribe();
-    this.newPassword = "";
-    this.newPasswordConfirm = "";
-    alert("Password changed successfully");}
+visibilityForm: boolean = false;
+visibilityButton: boolean = true;
+  changeP()
+  {
+    this.visibilityForm = true;
+    this.visibilityButton = false;
+  }
+  cancelP()
+  {
+    this.visibilityForm = false;
+    this.visibilityButton = true;
+  }
+  onStrengthChanged(strength: number) 
+  {
+    this.passwordStrength = strength;
+  }
+  saveChanges()
+  {
+    if(this.passwordStrength<40)
+      { 
+        alert("Your password must meet at least 2 of 5 conditions");
+      }
+    else
+      {
+        this.user.password = this.newPassword;
+        this.old = false;
+        this.checked = false;
+        this.userService.updateUser(this.user).subscribe();
+        this.newPassword = "";
+        this.newPasswordConfirm = "";
+        alert("Password changed successfully");
+      }
+  }
+  canSave(): boolean
+  {
+    if(this.passMatch() && this.newPassword != "")
+      {
+       return true;
+      }
+    else return false;
   }
   passMatch(): boolean
   {
-     if(this.newPassword == this.newPasswordConfirm && this.old){
+     if((this.newPassword == this.newPasswordConfirm && this.old) 
+         || this.newPassword == "")
+      {
        return true;
-     }
+      }
      else return false;
   }
-
-  checkOldPassword(pass: string){
-    let res = this.userService.checkPass(pass, this.id).subscribe(data=>this.old =data);
+  
+  checkOldPassword()
+  {
+    if(this.oldPassword != "")
+    {
+    let res = this.userService.checkPass(this.oldPassword, this.id).subscribe(data=>this.old =data);
     return res;
+    }
   }
   constructor(private userService: UserService){  }
 
-     ngOnInit() {
+     ngOnInit() 
+    {
     return this.userService.getUser(this.id).subscribe(data => this.user = data);
     }
 
