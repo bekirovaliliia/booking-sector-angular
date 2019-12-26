75232189f394ab4df523ac2a6acb90349ab678eb
Liliia Bekirova <lilybekirova@gmail.com>
2019-12-26 18:05:16 +0200

    added toaster to user profile

--- src/app/pages/profile-page/user-profile-text/user-profile-text.component.ts
+++ src/app/pages/profile-page/user-profile-text/user-profile-text.component.ts
@@ -4,7 +4,7 @@ import {User} from '../../../shared/models/user-model';
 import {UserService} from '../../../core/services/user.service';
 import { ChangePasswordNewComponent } from '../change-password-new/change-password-new.component';
 import { MatDialog, MatDialogRef } from '@angular/material';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-user-profile-text',
   templateUrl: './user-profile-text.component.html',
@@ -14,6 +14,9 @@ export class UserProfileTextComponent implements OnInit {
   is_edit : boolean = false;
   change_password: boolean = false;
   updateDialogRef: MatDialogRef<ChangePasswordNewComponent>;
+  showInfoEdited() {
+    this.toastr.success('Changes saved successfully!');
+  }
   openUpdateDialog() {
      this.updateDialogRef = this.dialog.open(ChangePasswordNewComponent, {
       hasBackdrop: false,
@@ -32,10 +35,11 @@ export class UserProfileTextComponent implements OnInit {
   saveChanges(){
     this.userService.updateUser(this.user).subscribe();
     this.is_edit = false;
-    alert("Changes saved successfully");
+    this.showInfoEdited();
   }
   constructor(private userService: UserService,
-              private dialog: MatDialog) { }  
+              private dialog: MatDialog,
+              private toastr: ToastrService) { }  
   user: User;  
   id = 46;
   ngOnInit() {
