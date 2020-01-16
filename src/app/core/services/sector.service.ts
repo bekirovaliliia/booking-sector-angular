500b727c86a916bb66c32ab591c5b17d7dd21b4e
Yura <41750763+zapletnyuk@users.noreply.github.com>
2020-01-16 19:08:04 +0200

    Added CRUD functionality to service

--- src/app/core/services/sector.service.ts
+++ src/app/core/services/sector.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Sector } from '../../shared/models/sector-model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class SectorService {
-
   constructor(private http: HttpClient) {}
+  public urlAddress: string = environment.urlAddress;
 
   getSectors(): Observable<Sector[]> {
-    return this.http.get<Sector[]>('https://localhost:44393/api/sectors')
+    return this.http.get<Sector[]>(`${this.urlAddress}sectors`)
       .pipe(
         map((data: Sector[]) =>
           data.map(
@@ -20,5 +21,35 @@ export class SectorService {
           )
         )
       );
+  }
+
+  getById(id: number): Observable<Sector> {
+    return this.http.get<Sector>(`${this.urlAddress}sectors/${id}`)
+      .pipe(
+        map((item: Sector) =>
+            new Sector(item.id, item.number, item.description, item.gpsLat, item.gpsLng, item.isActive)
+        )
+      );
+  }
+
+  add(sector: Sector): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    return this.http.post(`${this.urlAddress}sectors`, sector, httpOptions);
+  }
+
+  update(sector: Sector): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    return this.http.put(`${this.urlAddress}sectors/${sector.id}`, sector, httpOptions);
+  }
+
+  delete(id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    return this.http.delete(`${this.urlAddress}sectors/${id}`);
   }
 }
