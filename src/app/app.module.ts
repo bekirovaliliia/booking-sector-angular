26ae71c5fc3e495d4a6cd0f9100844416db7065e
DenysShevchuk <wev4yk2001@gmail.com>
2020-01-16 22:41:54 +0200

    fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -60,13 +60,10 @@ import { WithoutBookingsComponent } from './pages/user-bookings/without-bookings
 import { CalendarComponent } from './pages/admin-management/calendar/calendar.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-<<<<<<< HEAD
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
-=======
 import { HttpAuthInterceptor } from './core/interceptors/http-auth.interceptor';
 import { UserGuard } from './core/guards/user.guard';
 import { AdminGuard } from './core/guards/admin.guard';
->>>>>>> 010cbd2ddd0fd128634c22cb73a5591de49d4e26
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
     imports: [
