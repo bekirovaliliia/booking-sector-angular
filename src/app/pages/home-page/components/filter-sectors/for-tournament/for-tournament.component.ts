567d9923b9a2e65e9f3ab961003b0a52a7223403
Volodymyr Viniar <volodymyr.viniar@gmail.com>
2020-01-23 14:56:38 +0200

    fix date format in onChange method of for-tournament component

--- src/app/pages/home-page/components/filter-sectors/for-tournament/for-tournament.component.ts
+++ src/app/pages/home-page/components/filter-sectors/for-tournament/for-tournament.component.ts
@@ -37,14 +37,14 @@ export class ForTournamentComponent implements OnInit {
   }
   onChange(ob) {
     if (ob.value === null) {
-      this.selected = `${moment().format('YYYY/MM/DD') } - ${ moment().format('YYYY/MM/DD')  }`;
+      this.selected = `${moment().format('YYYY-MM-DD') } - ${ moment().format('YYYY-MM-DD')  }`;
       return;
     }
-    const dateStart = this.datePipe.transform(ob.value.tournamentStart, 'yyyy/MM/dd');
-    const dateEnd = this.datePipe.transform(ob.value.tournamentEnd, 'yyyy/MM/dd');
+    const dateStart = this.datePipe.transform(ob.value.tournamentStart, 'yyyy-MM-dd');
+    const dateEnd = this.datePipe.transform(ob.value.tournamentEnd, 'yyyy-MM-dd');
     
     this.selected = `${dateStart } - ${ dateEnd }`;
-    this.dataService.changeDateRange(ob.value.tournamentStart, ob.value.tournamentEnd);
+    this.dataService.changeDateRange(dateStart, dateEnd);
     this.dataService.selectedTournamentId = ob.value.id;
   }
 
