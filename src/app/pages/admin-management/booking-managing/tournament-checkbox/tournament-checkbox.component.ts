698140560c1955820c0a3213943ced94c0ec0db2
halyn <halyn@DESKTOP-7UHDIDH>
2020-02-05 09:53:29 +0200

    merge

--- src/app/pages/admin-management/booking-managing/tournament-checkbox/tournament-checkbox.component.ts
+++ src/app/pages/admin-management/booking-managing/tournament-checkbox/tournament-checkbox.component.ts
@@ -8,13 +8,17 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class TournamentCheckboxComponent implements OnInit {
  @Output()  selectedCheckbox: EventEmitter<boolean> = new EventEmitter<boolean>();
   selected: any;
+  isHidden: boolean;
   constructor() { }
-
   ngOnInit() {
     this.selectedCheckbox.emit(false);
   }
 
   onChange(selected) {
     this.selectedCheckbox.emit(selected);
+  }
+
+  hide() {
+    this.isHidden = (this.isHidden === true) ? false : true;
   }
 }
